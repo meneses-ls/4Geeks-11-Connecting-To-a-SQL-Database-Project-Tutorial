9b917c5984c7566ff897224cf7b16e69a750180f
meneses-ls <louismenesas@gmail.com>
2025-04-28 01:12:31 -0400

    Update appy.py

--- src/app.py
+++ src/app.py
@@ -3,15 +3,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
-
-#Código que se intentó
-#connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-#engine = create_engine(connection_string).execution_options(autocommit=True)
-#engine.connect()
 
 def connect():
     global engine 
@@ -23,8 +15,6 @@ def connect():
 
 connect()
 
-
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 engine.execute("""
 DROP TABLE IF EXISTS book_authors;
 DROP TABLE IF EXISTS books;
@@ -66,8 +56,6 @@ CREATE TABLE book_authors (
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );  """)
 
-
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 engine.execute("""  -- publishers
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
@@ -111,7 +99,5 @@ INSERT INTO book_authors (book_id, author_id) VALUES (8, 2);
 INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
 INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """)
 
-
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
 result_dataFrame = pd.read_sql("Select * from publishers;", engine)
 print(result_dataFrame.describe())
